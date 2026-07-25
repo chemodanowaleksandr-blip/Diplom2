92300f0b765f8acfac8fb586b51902560191fbfc
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-25 12:29:49 +0300

    Create test_login_user.py

---
+++ test_login_user.py
@@ -0,0 +1,22 @@
+import requests
+import allure
+from urls import BASE_URL
+
+@allure.epic("Stellar Burgers API")
+@allure.feature("Логин пользователя")
+class TestLoginUser:
+
+    @allure.story("Успешный вход под существующим пользователем")
+    def test_login_existing_user_success(self, created_user):
+        user_data, _ = created_user
+        login_data = {"email": user_data["email"], "password": user_data["password"]}
+        response = requests.post(f"{BASE_URL}/api/auth/login", json=login_data)
+        assert response.status_code == 200
+        assert response.json().get("success") is True
+
+    @allure.story("Ошибка при входе с неверным логином и паролем")
+    def test_login_wrong_credentials_error(self):
+        wrong_data = {"email": "invalid_user_999@yandex.ru", "password": "wrong_password"}
+        response = requests.post(f"{BASE_URL}/api/auth/login", json=wrong_data)
+        assert response.status_code == 401
+        assert response.json().get("success") is False
