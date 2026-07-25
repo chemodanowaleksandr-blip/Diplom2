cafe8658f7bed830b8af3c41b248ba920816782d
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-25 15:20:18 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,7 +6,6 @@ from urls import BASE_URL
 
 @pytest.fixture
 def user_data():
-    # Генерируем случайные данные для каждого теста
     random_str = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
     return {
         "email": f"alex_test_{random_str}@yandex.ru",
@@ -16,10 +15,8 @@ def user_data():
 
 @pytest.fixture
 def created_user(user_data):
-    # Создаем юзера перед тестом и отдаем данные
     response = requests.post(f"{BASE_URL}/api/auth/register", json=user_data)
     token = response.json().get("accessToken")
     yield user_data, token
-    # Удаляем юзера после теста (Teardown)
     if token:
         requests.delete(f"{BASE_URL}/api/auth/user", headers={"Authorization": token})
