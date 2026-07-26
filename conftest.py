829d517d16c454e0aa1cba72e84f7e02b7671577
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-26 13:39:43 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 import requests
-from urls import BASE_URL
+from data import Urls
 from helpers import generate_user_data
 
 @pytest.fixture
@@ -8,13 +8,13 @@ def created_user():
     """Фикстура создает уникального пользователя и удаляет его после теста"""
     payload = generate_user_data()
     
-    # Склеиваем BASE_URL с эндпоинтом регистрации
-    response = requests.post(f"{BASE_URL}/auth/register", json=payload)
+    # Берем эндпоинт REGISTER из класса Urls
+    response = requests.post(Urls.REGISTER, json=payload)
     
     token = response.json().get("accessToken")
     yield payload, token
     
-    # Очистка данных после теста
+    # Очистка данных после теста через эндпоинт USER
     if token:
         headers = {"Authorization": token}
-        requests.delete(f"{BASE_URL}/auth/user", headers=headers)
+        requests.delete(Urls.USER, headers=headers)
