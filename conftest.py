183e35f266296e9817a34d1b7e5a255c4e009d5c
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-26 11:50:50 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,22 +1,18 @@
 import pytest
 import requests
-import random
-import string
-from urls import BASE_URL
+from data import Urls
+from helpers import generate_user_data
 
 @pytest.fixture
-def user_data():
-    random_str = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
-    return {
-        "email": f"alex_test_{random_str}@yandex.ru",
-        "password": f"pass_{random_str}",
-        "name": f"Alex_{random_str}"
-    }
-
-@pytest.fixture
-def created_user(user_data):
-    response = requests.post(f"{BASE_URL}/api/auth/register", json=user_data)
+def created_user():
+    """Фикстура создает уникального пользователя и удаляет его после теста"""
+    payload = generate_user_data()
+    response = requests.post(Urls.REGISTER, json=payload)
+    
     token = response.json().get("accessToken")
-    yield user_data, token
+    yield payload, response
+    
+    # Очистка данных после теста
     if token:
-        requests.delete(f"{BASE_URL}/api/auth/user", headers={"Authorization": token})
+        headers = {"Authorization": token}
+        requests.delete(Urls.USER, headers=headers)
