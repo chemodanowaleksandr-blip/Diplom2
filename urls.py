820f2900006bbf817aca1643a6cf0ae1bba4f2fd
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-26 14:44:54 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,1 +1,8 @@
-BASE_URL = "https://nomoreparties.site"
+class Urls:
+    BASE_URL = "https://education-services.ru"
+    
+    REGISTER = f"{BASE_URL}/api/auth/register"
+    LOGIN = f"{BASE_URL}/api/auth/login"
+    USER = f"{BASE_URL}/api/auth/user"
+    ORDERS = f"{BASE_URL}/api/orders"
+    INGREDIENTS = f"{BASE_URL}/api/ingredients"
