c75882c20d3102cb2432164c84256b5612340d25
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-26 14:58:48 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 class Urls:
     BASE_URL = "https://nomoreparties.site"
     
-    REGISTER = f"{BASE_URL}/api/auth/register"
-    LOGIN = f"{BASE_URL}/api/auth/login"
-    USER = f"{BASE_URL}/api/auth/user"
+    REGISTER = f"{BASE_URL}/api/register"
+    LOGIN = f"{BASE_URL}/api/login"
+    USER = f"{BASE_URL}/api/user"
     ORDERS = f"{BASE_URL}/api/orders"
     INGREDIENTS = f"{BASE_URL}/api/ingredients"
