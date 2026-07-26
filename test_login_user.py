5975a68aea87f04e51aeccb58a5a788e93a0be46
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-26 11:56:47 +0300

    Update test_login_user.py

--- test_login_user.py
+++ test_login_user.py
@@ -10,13 +10,19 @@ class TestLoginUser:
     def test_login_existing_user_success(self, created_user):
         user_data, _ = created_user
         login_data = {"email": user_data["email"], "password": user_data["password"]}
-        response = requests.post(f"{BASE_URL}/api/auth/login", json=login_data)
+        
+        # Убран лишний /api из эндпоинта
+        response = requests.post(f"{BASE_URL}/auth/login", json=login_data)
+        
         assert response.status_code == 200
         assert response.json().get("success") is True
 
     @allure.story("Ошибка при входе с неверным логином и паролем")
     def test_login_wrong_credentials_error(self):
         wrong_data = {"email": "invalid_user_999@yandex.ru", "password": "wrong_password"}
-        response = requests.post(f"{BASE_URL}/api/auth/login", json=wrong_data)
+        
+        # Убран лишний /api из эндпоинта
+        response = requests.post(f"{BASE_URL}/auth/login", json=wrong_data)
+        
         assert response.status_code == 401
         assert response.json().get("success") is False
