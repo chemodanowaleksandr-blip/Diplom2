b79ee53e4b085242377bc1bc5723388c1a165ad1
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-26 14:14:11 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,16 +5,17 @@ from helpers import generate_user_data
 
 @pytest.fixture
 def created_user():
-    """Фикстура создает уникального пользователя и удаляет его после теста"""
     payload = generate_user_data()
-    
-    # Берем эндпоинт REGISTER из класса Urls
     response = requests.post(Urls.REGISTER, json=payload)
     
-    token = response.json().get("accessToken")
+    # Если сервер забанил IP гитхаба, не пытаемся парсить пустой JSON
+    if response.status_code == 429:
+        token = None
+    else:
+        token = response.json().get("accessToken") if response.status_code == 200 else None
+        
     yield payload, token
     
-    # Очистка данных после теста через эндпоинт USER
     if token:
         headers = {"Authorization": token}
         requests.delete(Urls.USER, headers=headers)
