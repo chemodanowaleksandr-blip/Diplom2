ac9d7d9f87d5075321d7afd426b8863e571f2b01
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-26 11:49:50 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,11 @@
+class Urls:
+    BASE_URL = "https://education-services.ru"
+    
+    # Пользователь
+    REGISTER = f"{BASE_URL}/auth/register"
+    LOGIN = f"{BASE_URL}/auth/login"
+    USER = f"{BASE_URL}/auth/user"
+    
+    # Заказы
+    ORDERS = f"{BASE_URL}/orders"
+    INGREDIENTS = f"{BASE_URL}/ingredients"
