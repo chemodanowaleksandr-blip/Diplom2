08ad9de354722e2605f4d18042d657110d94d8bb
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-26 16:49:52 +0300

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-class Urls:
-    BASE_URL = "https://nomoreparties.site"
-    
-    REGISTER = f"{BASE_URL}/api/register"
-    LOGIN = f"{BASE_URL}/api/login"
-    USER = f"{BASE_URL}/api/user"
-    ORDERS = f"{BASE_URL}/api/orders"
-    INGREDIENTS = f"{BASE_URL}/api/ingredients"
