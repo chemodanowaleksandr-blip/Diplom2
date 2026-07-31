017e600505ce8623697c4a87bc7f834376e8f117
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-31 11:51:39 +0300

    Update test_login_user.py

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -1,6 +1,7 @@
+import pytest
 import requests
 import allure
-from data import Urls
+from data import urls
 
 @allure.epic("Stellar Burgers API")
 @allure.feature("Логин пользователя")
@@ -11,22 +12,18 @@ class TestLoginUser:
         user_data, _ = created_user
         login_data = {"email": user_data["email"], "password": user_data["password"]}
         
-        response = requests.post(Urls.LOGIN, json=login_data)
-        
-        # Если сервер забанен (429), мы пропускаем тест, чтобы сборка не падала
-        if response.status_code == 429:
-            assert True
-        else:
-            assert response.status_code == 200
-            assert response.json().get("success") is True
+        with allure.step("Отправка POST-запроса на логин пользователя"):
+            response = requests.post(urls.LOGIN, json=login_data)
+
+        assert response.status_code == 200
+        assert response.json().get("success") is True
 
     @allure.story("Ошибка при входе с неверным логином и паролем")
     def test_login_wrong_credentials_error(self):
         wrong_data = {"email": "invalid_user_999@yandex.ru", "password": "wrong_password"}
-        
-        response = requests.post(Urls.LOGIN, json=wrong_data)
-        
-        if response.status_code == 429:
-            assert True
-        else:
-            assert response.status_code == 401
+
+        with allure.step("Отправка POST-запроса с неверными кредами"):
+            response = requests.post(urls.LOGIN, json=wrong_data)
+
+        assert response.status_code == 401
+        assert response.json().get("success") is False
