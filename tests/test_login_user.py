98548e6157e9bdf652a06394da91179f3b7c53c3
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-31 12:15:35 +0300

    Update test_login_user.py

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -1,7 +1,14 @@
 import pytest
 import requests
 import allure
+import sys
+import os
+
+# Этот кусочек кода научит Python видеть файлы в корне проекта
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from data import urls
+
 
 @allure.epic("Stellar Burgers API")
 @allure.feature("Логин пользователя")
@@ -10,8 +17,11 @@ class TestLoginUser:
     @allure.story("Успешный вход под существующим пользователем")
     def test_login_existing_user_success(self, created_user):
         user_data, _ = created_user
-        login_data = {"email": user_data["email"], "password": user_data["password"]}
-        
+        login_data = {
+            "email": user_data["email"],
+            "password": user_data["password"],
+        }
+
         with allure.step("Отправка POST-запроса на логин пользователя"):
             response = requests.post(urls.LOGIN, json=login_data)
 
@@ -20,7 +30,10 @@ class TestLoginUser:
 
     @allure.story("Ошибка при входе с неверным логином и паролем")
     def test_login_wrong_credentials_error(self):
-        wrong_data = {"email": "invalid_user_999@yandex.ru", "password": "wrong_password"}
+        wrong_data = {
+            "email": "invalid_user_999@yandex.ru",
+            "password": "wrong_password",
+        }
 
         with allure.step("Отправка POST-запроса с неверными кредами"):
             response = requests.post(urls.LOGIN, json=wrong_data)
