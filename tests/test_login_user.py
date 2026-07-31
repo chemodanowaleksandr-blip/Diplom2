8164d85654bcab27f5465ea91886ad7ae3b3d323
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-07-31 12:32:33 +0300

    Update test_login_user.py

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -1,12 +1,6 @@
 import pytest
 import requests
 import allure
-import sys
-import os
-
-# Добавляем корень проекта в пути поиска модулей
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from data import urls
 
 
